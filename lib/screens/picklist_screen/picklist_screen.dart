b559df333d1fa03b1152e20d0a21efdced87f32b
Igor Gaponov <jiminy96@gmail.com>
2021-06-11 14:03:44 -0500

    Add filter by ean for products

--- lib/screens/picklist_screen/picklist_screen.dart
+++ lib/screens/picklist_screen/picklist_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +5,7 @@ import 'package:scanner/api.dart';
 import 'package:scanner/models/picklist.dart';
 import 'package:scanner/models/picklist_line.dart';
 import 'package:scanner/screens/picklist_screen/widgets/picklist_item.dart';
+import 'package:scanner/screens/picklist_screen/widgets/product_list.dart';
 import 'package:scanner/widgets/wms_app_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -48,47 +47,7 @@ class _PicklistScreenState extends State<PicklistScreen> {
             return CustomScrollView(
               slivers: [
                 PicklistItem(picklist),
-                SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    final line = lines[index];
-                    return Column(
-                      children: [
-                        ListTile(
-                          onTap: () {
-                            Navigator.of(context).pushNamed('/product', arguments: line);
-                          },
-                          leading: Container(
-                            width: 60,
-                            height: 60,
-                            color: Colors.grey[300],
-                            child: FutureBuilder<Response<Uint8List>>(
-                              future: getProductImage(line.product.id),
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  return Image.memory(snapshot.data!.data!);
-                                }
-                                return Center(child: Text(line.product.uid.substring(0, 1)));
-                              },
-                            ),
-                          ),
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(line.product.uid, style: TextStyle(fontSize: 13)),
-                              Text(line.product.description, style: TextStyle(fontSize: 16), overflow: TextOverflow.ellipsis,),
-                              Text('${line.pickAmount} x ${line.product.unit}'),
-                              Text('Floor 2 - Row 14', style: TextStyle(fontSize: 13, color: Colors.grey[400],)),
-                            ],
-                          ),
-                          trailing: Icon(Icons.chevron_right),
-                        ),
-                        Divider(height: 5),
-                      ],
-                    );
-                  },
-                  childCount: lines.length,
-                )),
+                ProductList(lines),
               ],
             );
           }
---
+++ lib/screens/picklist_screen/widgets/product_list.dart
@@ -0,0 +1,88 @@
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:scanner/api.dart';
+import 'package:scanner/models/picklist_line.dart';
+import 'package:scanner/widgets/barcode_input.dart';
+
+class ProductList extends StatefulWidget {
+  final List<PicklistLine> lines;
+
+  const ProductList(this.lines, {Key? key}) : super(key: key);
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  String _ean = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+        delegate: SliverChildListDelegate([
+              Column(
+                children: [
+                  ListTile(
+                    title: BarcodeInput((value, barcode) {
+                      setState(() {
+                        _ean = value;
+                      });
+                    }),
+                  ),
+                  Divider(),
+                ],
+              ),
+            ].toList() +
+            widget.lines
+                .where((line) => _ean == '' || line.product.ean == _ean)
+                .map((line) => Column(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.of(context)
+                                .pushNamed('/product', arguments: line);
+                          },
+                          leading: Container(
+                            width: 60,
+                            height: 60,
+                            color: Colors.grey[300],
+                            child: FutureBuilder<Response<Uint8List>>(
+                              future: getProductImage(line.product.id),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Image.memory(snapshot.data!.data!);
+                                }
+                                return Center(
+                                    child:
+                                        Text(line.product.uid.substring(0, 1)));
+                              },
+                            ),
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(line.product.uid,
+                                  style: TextStyle(fontSize: 13)),
+                              Text(
+                                line.product.description,
+                                style: TextStyle(fontSize: 16),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text('${line.pickAmount} x ${line.product.unit}'),
+                              Text('Floor 2 - Row 14',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    color: Colors.grey[400],
+                                  )),
+                            ],
+                          ),
+                          trailing: Icon(Icons.chevron_right),
+                        ),
+                        Divider(height: 5),
+                      ],
+                    ))
+                .toList()));
+  }
+}
