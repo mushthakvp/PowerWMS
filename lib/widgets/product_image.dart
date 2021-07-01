1f45a9c67189d0b175437e001c5ac3a9ca93c260
Igor Gaponov <jiminy96@gmail.com>
2021-07-01 10:06:08 +0500

    Fix product image loading

--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -5,18 +5,35 @@ import 'package:scanner/screens/resources/product_image_repository.dart';
 
 final productImageRepository = ProductImageRepository();
 
-class ProductImage extends StatelessWidget {
+class ProductImage extends StatefulWidget {
   final int productId;
   final double? width;
 
   const ProductImage(this.productId, {Key? key, this.width}) : super(key: key);
 
   @override
+  _ProductImageState createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  Future<Uint8List?>? _future;
+
+  @override
+  void didChangeDependencies() {
+    _future = productImageRepository.getImageFile(widget.productId);
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<Uint8List?>(
-      future: productImageRepository.getImageFile(productId),
+      future: _future!,
       builder: (context, snapshot) {
-        var fallback = Container(width: width, height: width, color: Colors.grey[400]);
+        var fallback = Container(
+          width: widget.width,
+          height: widget.width,
+          color: Colors.grey[400],
+        );
         if (snapshot.hasError) {
           print(snapshot.error);
         }
@@ -25,7 +42,7 @@ class ProductImage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 10),
             child: Image.memory(
               snapshot.data!,
-              width: width ?? double.infinity,
+              width: widget.width ?? double.infinity,
               errorBuilder: (context, error, _) => fallback,
             ),
           );
