c73c62684eb2de937d0b4881460e123a04167018
TRAN DAT <nartjie11092@gmail.com>
2022-08-27 10:27:47 +0700

    feat: implement enlarge product image

--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -38,17 +38,84 @@ class _ProductImageState extends State<ProductImage> {
           print(snapshot.error);
         }
         if (snapshot.hasData) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Image.memory(
-              snapshot.data!,
-              width: widget.width ?? double.infinity,
-              errorBuilder: (context, error, _) => fallback,
+          return InkWell(
+            onTap: () {
+              OptionPage().showOption(context, snapshot.data!);
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Image.memory(
+                snapshot.data!,
+                width: widget.width ?? double.infinity,
+                errorBuilder: (context, error, _) => fallback,
+              ),
             ),
           );
         }
         return fallback;
       },
     );
+  }
+}
+
+class OptionPage {
+  /// Show Option
+  Future<void> showOption(BuildContext context, Uint8List data) async {
+    showDialog<dynamic>(
+        context: context,
+        barrierColor: Colors.black.withOpacity(.4),
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          _dialogContext = context;
+          return Dialog(
+            insetPadding: EdgeInsets.symmetric(horizontal: 32),
+            elevation: 8,
+            child: Container(
+              color: Colors.white,
+              height: 0.6 * MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 16,
+                    right: 16,
+                    child: InkWell(
+                      onTap: hideOption,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        child: Icon(Icons.close, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.all(8),
+                      child: Image.memory(
+                        data,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+    );
+  }
+
+  /// BuildContext
+  BuildContext? _dialogContext;
+
+  void hideOption() {
+    if (_dialogContext != null) {
+      if (Navigator.canPop(_dialogContext!)) {
+        Navigator.pop(_dialogContext!);
+      }
+      _dialogContext = null;
+    }
   }
 }
